a0f5aa98e9e865ad3c94719f760996e01ff3bb60
Joel Noyce <noyce.joel@gmail.com>
2024-02-21 21:23:03 +0000

    use client in fetch

--- src/app/services/fetch.tsx
+++ src/app/services/fetch.tsx
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 export default function getAllEvents() {
-    const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || 'https://life-drawing.vercel.app';
+    const baseUrl = 'https://life-drawing.vercel.app';
   return axios.get(`${baseUrl}/api/events`)
     .then(response => response.data)
     .catch(error => {
