b93f74f129cc72f7d054c068e157f3b1753e5bc7
Joel Noyce <noyce.joel@gmail.com>
2024-02-21 20:34:02 +0000

    added absolute url to api routes fix

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
 
 type ResponseData = {
   message: string;
@@ -8,8 +9,8 @@ type ResponseData = {
 const key = process.env.EVENTBRITE_API_KEY;
 
 export async function GET(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  req: NextRequest,
+  res: NextResponse<ResponseData>
 ) {
   const organisation_id =  2017829680413;
   const eventsUrl = `https://www.eventbriteapi.com/v3/organizations/${organisation_id}/events/`;
@@ -18,17 +19,17 @@ export async function GET(
     Authorization: `Bearer ${key}`,
     "Content-Type": "application/json",
   };
-  const options = {
+const options = {
     method: "GET",
     headers: headerParameters,
-  };
-  
-    const response = await axios.get(eventsUrl, options)
+};
+
+const response = await axios.get(eventsUrl, options)
     .then((response) => {
-      res.status(200).json(response.data);
+        res.status(200).json(response.data);
     })
     .catch((error) => {
-      res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message });
     });
     
 }
