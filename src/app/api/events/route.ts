9452c6686917f6d04fd0b350e618b4c7e98ca9df
Joel Noyce <noyce.joel@gmail.com>
2024-02-21 20:41:12 +0000

    converted to use NextREsponse and NextREquest as requested by vercel deployment logs

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,34 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
-
-type ResponseData = {
-  message: string;
-};
+import { NextRequest, NextResponse } from 'next/server';
+import axios from 'axios';
 
 const key = process.env.EVENTBRITE_API_KEY;
+const organisation_id = 2017829680413;
+const eventsUrl = `https://www.eventbriteapi.com/v3/organizations/${organisation_id}/events/`;
 
-export async function GET(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
-  const organisation_id =  2017829680413;
-  const eventsUrl = `https://www.eventbriteapi.com/v3/organizations/${organisation_id}/events/`;
+const headerParameters = {
+  Authorization: `Bearer ${key}`,
+  "Content-Type": "application/json",
+};
 
-  const headerParameters = {
-    Authorization: `Bearer ${key}`,
-    "Content-Type": "application/json",
-  };
+export async function middleware(req: NextRequest) {
   const options = {
     method: "GET",
     headers: headerParameters,
   };
-  
-    const response = await axios.get(eventsUrl, options)
-    .then((response) => {
-      res.status(200).json(response.data);
-    })
-    .catch((error) => {
-      res.status(500).json({ message: error.message });
-    });
-    
+
+  try {
+    const response = await axios.get(eventsUrl, options);
+    return NextResponse.json(response.data);
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message })
+  }
 }
