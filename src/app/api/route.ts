7d4dbabfa113e7610feec9e2fb693b987afd57be
Joel Noyce <noyce.joel@gmail.com>
2024-02-21 18:31:58 +0000

    setup

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,1 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
  
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    res.send('Hello from the server')
+  } else {
+    res.json({ message: 'Method not allowed' })
+  }
+}
