5ec95456e7b0eb966bb9158212d9bd3701ae445c
Joel Noyce <noyce.joel@gmail.com>
2024-02-28 19:42:13 +0000

    width issues

--- src/app/components/events/Events.tsx
+++ src/app/components/events/Events.tsx
@@ -72,7 +72,7 @@ export default function Events() {
       <article className="flex flex-col pt-24 md:pt-32 justify-end items-end h-auto font-extrabold text-[90px] ">
         <div className="flex w-3/4 md:w-4/5 relative flex-col font-bold text-[30px] md:text-[70px]  text-white">
           EVENTS
-          <motion.div
+          {/* <motion.div
             initial={{ scale: 0 }}
             whileInView={{
               scale: 1,
@@ -85,7 +85,7 @@ export default function Events() {
               },
             }}
             className=" -z-40 origin-right md:-mt-5 border-b border-[1.2px]"
-          ></motion.div>
+          ></motion.div> */}
         </div>
 
         
@@ -96,7 +96,7 @@ export default function Events() {
           <div onClick={() => setIsOpen(!isOpen)} className=" hover:cursor-pointer hover:scale-[104%] pb-2 hover:text-gray-900 transition-all duration-200 ">
           SHEFFIELD
           </div>
-          <motion.div
+          {/* <motion.div
             initial={{ scale: 0 }}
             whileInView={{
               scale: 1,
@@ -109,7 +109,7 @@ export default function Events() {
               },
             }}
             className="-z-40  origin-right md:-mt-5 border-b border-[1.2px]"
-          ></motion.div>
+          ></motion.div> */}
         </div>
       </article>
 
