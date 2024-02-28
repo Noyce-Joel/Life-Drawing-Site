a10f7cf91ff1aee7a9222dbf6f35ee0a98e1fa43
Joel Noyce <noyce.joel@gmail.com>
2024-02-28 19:24:15 +0000

    width test

--- src/app/components/events/Events.tsx
+++ src/app/components/events/Events.tsx
@@ -72,7 +72,7 @@ export default function Events() {
       <article className="flex flex-col pt-24 md:pt-32 justify-end items-end h-auto font-extrabold text-[90px] ">
         <div className="flex w-3/4 md:w-4/5 relative flex-col font-bold text-[30px] md:text-[70px]  text-white">
           EVENTS
-          <motion.div
+          {/* <motion.div
             initial={{ scale: 0 }}
             whileInView={{
               scale: 1,
@@ -85,7 +85,7 @@ export default function Events() {
               },
             }}
             className=" -z-40 origin-right md:-mt-5 border-b border-[1.2px]"
-          ></motion.div>
+          ></motion.div> */}
         </div>
 
         <div className="flex justify-center relative md:w-4/5 items-center md:justify-center md:items-center pt-6 pb-12 md:pb-0 md:pt-16">
@@ -103,7 +103,7 @@ export default function Events() {
           <div onClick={() => setIsOpen(!isOpen)} className=" hover:cursor-pointer hover:scale-[104%] pb-2 hover:text-gray-900 transition-all duration-200 ">
           SHEFFIELD
           </div>
-          <motion.div
+          {/* <motion.div
             initial={{ scale: 0 }}
             whileInView={{
               scale: 1,
@@ -116,7 +116,7 @@ export default function Events() {
               },
             }}
             className="-z-40  origin-right md:-mt-5 border-b border-[1.2px]"
-          ></motion.div>
+          ></motion.div> */}
         </div>
       </article>
 
