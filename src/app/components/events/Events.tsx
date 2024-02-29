68b697d8c944582de173ce3a3f1e2f1e276cb1d3
Joel Noyce <noyce.joel@gmail.com>
2024-02-29 13:17:54 +0000

    prepared the site for the client to view

--- src/app/components/events/Events.tsx
+++ src/app/components/events/Events.tsx
@@ -5,6 +5,7 @@ import { StateContext, StateContextType } from "../../context/State";
 import { Dialog } from "@headlessui/react";
 import Event from "./Event";
 import Line from "../line/Line";
+import EventsList from "./EventsList";
 
 declare global {
   interface Window {
@@ -55,67 +56,20 @@ function Modal() {
 }
 
 export default function Events() {
-  const { events } = useContext<StateContextType>(StateContext);
-  const [modalEventId, setModalEventId] = useState<number>(0);
-  const [isOpen, setIsOpen] = useState(false);
-  const handleEventClick = (id: number) => {
-    setModalEventId(id);
-  };
+  
 
-  console.log(events);
+  
   return (
     <>
-      <article className="flex flex-col pt-24 md:pt-32 justify-center items-center h-auto font-extrabold text-[90px] ">
-        <div className="flex w-full pl-12 lg:pl-72 relative flex-col font-bold text-[30px] md:text-[75px]  text-white">
+    <div className="flex w-full pl-12 lg:pl-72 relative flex-col pt-24 font-bold text-[30px] md:text-[75px]  text-white">
           EVENTS
           <Line title={false} />
         </div>
+      
+        <EventsList city='SHEFFIELD' />
+        <EventsList city='NOTTINGHAM' />
 
-        <div
-          onClick={() => setIsOpen(!isOpen)}
-          className="flex w-3/4 md:w-3/5 relative flex-col font-semibold text-[30px] md:text-[50px] text-white md:pt-12 hover:cursor-pointer hover:scale-[104%] hover:text-gray-900 transition-all duration-200"
-        >
-          SHEFFIELD
-        </div>
-        <div className="w-full flex relative ">
-          <Line title={false} />
-        </div>
-      </article>
-
-      <section
-        className={classNames(
-          isOpen
-            ? "h-[41rem] w-full"
-            : "h-[2rem] md:w-[20rem] lg:w-[60rem] flex ",
-          "overflow-hidden relative transition-all duration-1000 ease-in-out origin-top-left border-gray-900 border-b-[1.2px] rounded-br-3xl border-r-[1.2px] "
-        )}
-      >
-        <div className="absolute top-0 w-full flex overflow-x-auto gap-12 lg:gap-24 justify-between md:items-end p-24">
-          {events.map((event, index) => {
-            const id = event.id;
-            const name = event.name.text;
-            const start = event.start.local;
-            const end = event.end.local;
-            const description = event.description.text;
-            const logo = event.logo.original.url;
-            const capacity = event.capacity;
-            return (
-              <div key={index} className="z-10 ">
-                <Event
-                  eventClick={handleEventClick}
-                  id={id}
-                  name={name}
-                  start={start}
-                  end={end}
-                  description={description}
-                  capacity={capacity}
-                  logo={logo}
-                />
-              </div>
-            );
-          })}
-        </div>
-      </section>
+        
     </>
   );
 }
---
+++ src/app/components/events/EventsList.tsx
@@ -0,0 +1,65 @@
+import { StateContext, StateContextType } from '@/app/context/State';
+import React, { useContext, useState } from 'react'
+import Line from '../line/Line';
+import Event from './Event';
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+export default function EventsList({city}: {city: string}) {
+    const { events } = useContext<StateContextType>(StateContext);
+  const [modalEventId, setModalEventId] = useState<number>(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleEventClick = (id: number) => {
+    setModalEventId(id);
+  };
+  return (
+   <>
+   <article className="flex flex-col pt-12 justify-center items-center h-auto font-extrabold text-[90px] ">
+   <div
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex w-3/4 md:w-3/5 relative flex-col font-semibold text-[30px] md:text-[50px] text-white md:pt-12 hover:cursor-pointer hover:scale-[104%] hover:text-gray-900 transition-all duration-200"
+        >
+         {city}
+        </div>
+        <div className="w-full flex relative ">
+          <Line title={false} />
+        </div>
+      </article>
+
+      <section
+        className={classNames(
+          isOpen
+            ? "h-[41rem] w-full"
+            : "h-[2rem] md:w-[20rem] lg:w-[60rem] flex ",
+          "overflow-hidden relative transition-all duration-1000 ease-in-out origin-top-left border-gray-900 border-b-[1.2px] rounded-br-3xl border-r-[1.2px] "
+        )}
+      >
+        <div className="absolute top-0 w-full flex overflow-x-auto gap-12 lg:gap-24 justify-between md:items-end p-24">
+          {events.map((event, index) => {
+            const id = event.id;
+            const name = event.name.text;
+            const start = event.start.local;
+            const end = event.end.local;
+            const description = event.description.text;
+            const logo = event.logo.original.url;
+            const capacity = event.capacity;
+            return (
+              <div key={index} className="z-10 ">
+                <Event
+                  eventClick={handleEventClick}
+                  id={id}
+                  name={name}
+                  start={start}
+                  end={end}
+                  description={description}
+                  capacity={capacity}
+                  logo={logo}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+   </>
+  )
+}
