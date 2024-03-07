81cb102576ca7185d56e39b8fdf1696bed5e82eb
Joel Noyce <noyce.joel@gmail.com>
2024-03-07 18:35:56 +0000

    email fix with try/catch

--- src/app/services/mail.tsx
+++ src/app/services/mail.tsx
@@ -1,8 +1,8 @@
-"use server";
+'use server'
 
 import nodemailer from "nodemailer";
 
-export default async function sendMail({
+export async function sendMail({
   to,
   name,
   subject,
@@ -11,46 +11,37 @@ export default async function sendMail({
 }: {
   to: string;
   name: string;
-  email: string;
+  email: string,
   subject: string;
   body: string;
 }) {
-  const { SMTP_EMAIL, SMTP_PASSWORD } = process.env;
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: SMTP_EMAIL,
-      pass: SMTP_PASSWORD,
-    },
-    secure: true,
-  });
-  await new Promise((resolve, reject) => {
-    // verify connection configuration
-    transporter.verify(function (error, success) {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        console.log("Server is ready to take our messages");
-        resolve(success);
-      }
-    });
-  });
-
-  await new Promise((resolve, reject) => {
-    const send = transporter.sendMail({
-      from: SMTP_EMAIL,
-      to,
-      subject,
-      html: body,
-    }, (err, info) => {
-        if (err) {
-            reject(err);
-          } else {
-            resolve(info);
-          }
+    const {SMTP_EMAIL, SMTP_PASSWORD} = process.env;
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: SMTP_EMAIL,
+            pass: SMTP_PASSWORD
+        },
+    })
+    try {
+        const test = await transporter.verify()
+        console.log(test)
+    } catch(err) {
+        console.log(err)
+        return;
     }
-    );
-   
-  });
+
+    try {
+        const send = await transporter.sendMail({
+            from: SMTP_EMAIL,
+            to,
+            subject,
+            html: body,
+        })
+        console.log(send)
+    } catch(err) {
+        console.log(err)
+    }
 }
+
+
