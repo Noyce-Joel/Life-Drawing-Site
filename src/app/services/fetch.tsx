291c348a384444f1354de2feab85d9a30ffa64a7
Joel Noyce <noyce.joel@gmail.com>
2024-06-27 17:31:22 +0100

    changed baseUrl of axios request again

--- src/app/services/fetch.tsx
+++ src/app/services/fetch.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export default function getAllEvents() {
 
   const baseUrl =
-    "https://www.doglifedrawing.com" || process.env.NEXT_PUBLIC_VERCEL_URL;
+    "https://doglifedrawing.com" || process.env.NEXT_PUBLIC_VERCEL_URL;
   return axios
     .get(`${baseUrl}/api/events`)
     .then((response) => response.data)
