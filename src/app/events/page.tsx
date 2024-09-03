f7b0e2eebec38feaf1730cff76e95d4e5c92a830
Joel Noyce <noyce.joel@gmail.com>
2024-09-03 15:39:05 +0100

    added structured markup for improved seo

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -7,8 +7,16 @@ import { motion } from "framer-motion";
 import Line from "../components/line/Line";
 import Nav from "../components/nav/Nav";
 import Title from "../components/hero/Title";
+import { copy } from "../../../public/copy/copy";
 
 export default function page() {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Event",
+    name: "Dog Life Drawing",
+    image: "/images/faviconLogo.png",
+    description: `${copy.sessions_white_text}`,
+  };
   const title = ["DOG LIFE", "DRAWING", "EVENTS"];
   const container = {
     animate: {
@@ -24,27 +32,39 @@ export default function page() {
     animate: { y: 0, x: 0, rotate: 0, transition: { duration: 0.7 } },
   };
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { duration: 1, delay: 0.4 } }}
-      className=" h-screen relative overflow-y-auto overflow-x-hidden"
-    >
-      <Nav
-        color="text-[#000000]"
-        hoverColor="text-[#DA7835]"
-        socials="#000000"
-        socialsHover="#DA7835"
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
-     <Title slogan='Come along and have some fun' image='/drawingLow.jpg' image2='/Dog-Model2Low.jpeg' title={['OUR', '', 'EVENTS']} tileColor='#334D42'/>
-
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { duration: 1, delay: 0.4 } }}
-        className="pb-44"
+        className=" h-screen relative overflow-y-auto overflow-x-hidden"
       >
-        <Events comp={false} />
+        <Nav
+          color="text-[#000000]"
+          hoverColor="text-[#DA7835]"
+          socials="#000000"
+          socialsHover="#DA7835"
+        />
+        <Title
+          slogan="Come along and have some fun"
+          image="/drawingLow.jpg"
+          image2="/Dog-Model2Low.jpeg"
+          title={["OUR", "", "EVENTS"]}
+          tileColor="#334D42"
+        />
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1, delay: 0.4 } }}
+          className="pb-44"
+        >
+          <Events comp={false} />
+        </motion.div>
+        <Footer />
       </motion.div>
-      <Footer />
-    </motion.div>
+    </>
   );
 }
