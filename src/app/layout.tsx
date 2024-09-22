e19b463e1b0d9eb8133a0c6ca75438e32435ffda
Joel Noyce <noyce.joel@gmail.com>
2024-09-22 12:41:44 +0100

    events fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import { Fraunces } from "next/font/google";
 import { StateProvider } from "./context/State";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import Head from "next/head";
-import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+
 const inter = Fraunces({
   subsets: ["latin"],
   weight: ["400"],
@@ -14,21 +13,7 @@ export const metadata: Metadata = {
   title: "Dog Life Drawing",
   description:
     "Our dog life drawing sessions blend the joy of artistic expression with the undeniable charm of our favourite furry friends. Whether you're an experienced artist or a complete novice, our sessions cater to all skill levels. ",
-  keywords: [
-    "anomaly",
-    "dogs",
-    "drawing",
-    "art",
-    "life drawing",
-    "Sheffield",
-    "Nottingham",
-    "York",
-    "Leeds",
-    "life",
-    "events",
-    "Leeds",
-    "workshop",
-  ],
+ 
   icons: "/images/faviconLogo.png",
   openGraph: {
     title: "Dog Life Drawing",
@@ -46,8 +31,6 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
-        <GoogleTagManager gtmId="AW-16703303348" />
-
         <body className={inter.className}>
           <Analytics />
           <StateProvider>{children}</StateProvider>
