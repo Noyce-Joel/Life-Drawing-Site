e7628abe4fe9db3221a57bc8b0367322fa9ea51b
Joel Noyce <noyce.joel@gmail.com>
2024-10-04 13:45:20 +0100

    added a sitemap with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.doglifedrawing.com',
+    generateRobotsTxt: true, 
+    changefreq: 'monthly',
+    priority: 1,
+  }
